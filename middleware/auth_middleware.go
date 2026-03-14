fe6ae8d56bb516c56f542ed2f6d6b4a7882be752
RIOKOWI <achyarrio2004@gmail.com>
2026-03-15 01:25:17 +0700

    feat(auth middleware): create error handling for func get token from header authorization

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -1,12 +1,22 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/s2a-go/retry"
+)
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func (c *gin.Context){
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"success": false,
+				"message": "gak nemu Authorization",
+				"error_code": "MISSING_TOKEN",
+			})
+			return
 		}
 	}
 }
