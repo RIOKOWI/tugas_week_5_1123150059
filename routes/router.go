5aa0c0847c6a5521d14b10bb019a8df51c0f65ff
RIOKOWI <achyarrio2004@gmail.com>
2026-03-15 19:33:13 +0700

    feat(product handler): CORS allowed request

---
+++ routes/router.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"github.com/RIOKOWI/tugas_week_5_1123150059/handlers"
+	"github.com/RIOKOWI/tugas_week_5_1123150059/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+}
