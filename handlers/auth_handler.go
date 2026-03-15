c33aa7117b73f669e8d40a5baa8a1c3f313e791c
RIOKOWI <achyarrio2004@gmail.com>
2026-03-15 19:09:33 +0700

    feat(auth handler): parse request body

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -1,13 +1,30 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/RIOKOWI/tugas_week_5_1123150059/services"
+	"github.com/gin-gonic/gin"
 )
 
 
 type AuthHandler struct {
 	authService *services.AuthService
 }
+
 func NewAuthHandler() *AuthHandler {
 	return &AuthHandler{authService: services.NewAuthService()}
+}
+
+func (h *AuthHandler) VerifyToken(c *gin.Context) {
+	var req struct {
+		FirebaseToken string `json:"firebase_token" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"message": "firebase_token wajib diisi",
+		})
+		return
+	}
 }
