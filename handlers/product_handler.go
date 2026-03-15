fd4e7a8fbbb138b5ac4fd596026ade774ceb7011
RIOKOWI <achyarrio2004@gmail.com>
2026-03-15 19:20:00 +0700

    feat(product handler): create API Get All with pagination

--- handlers/product_handler.go
+++ handlers/product_handler.go
@@ -1,8 +1,12 @@
-package handlers 
+package handlers
 
-import(
+import (
+	"net/http"
+	"strconv"
+
 	"github.com/RIOKOWI/tugas_week_5_1123150059/models"
 	"github.com/RIOKOWI/tugas_week_5_1123150059/services"
+	"github.com/gin-gonic/gin"
 )
 
 
@@ -12,5 +16,29 @@ type ProductHandler struct {
 
 func NewProductHandler() *ProductHandler {
 	return &ProductHandler{productService: services.NewProductService()}
+}
+
+func (h *ProductHandler) GetAll(c *gin.Context) {
+	page, _ := strconv.Atoi(c.DefaultQuery("page", "1"))
+	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "10"))
+	category := c.Query("category")
+	products, total, err := h.productService.GetAll(page, limit, category)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"success": false, "message": "Gagal mengambil data produk",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data": products,
+		"meta": gin.H{
+			"total": total,
+			"page": page,
+			"limit": limit,
+			"per_page": limit,
+		},
+	})
 }
 
